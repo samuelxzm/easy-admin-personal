8b74a3e429b73f617839c168caffd6a6e9fd4368
xzm <speme_x@126.com>
2019-10-30 09:41:42 +0800

    增加无框架命令

--- pages/main.js
+++ pages/main.js
@@ -5,13 +5,10 @@ import Ea from 'easy-admin-framework'
 import 'easy-admin-framework/lib/veasyadmin.css'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import { Object } from 'core-js'
-
 Vue.config.productionTip = false
 Vue.use(elementUI)
 Vue.use(Ea)
 // document.title = 'whatever you want'
-
 // requires and returns all modules that match
 const requireAll = requireContext => requireContext.keys().map(requireContext);
 // import all svg
@@ -23,22 +20,28 @@ global.menu = routerList.routerList
 let Routerdata = JSON.parse(JSON.stringify(routerList))
 
 function setLayer(obj) {
-    return obj.map(e => {
-        let a = e
-        let path = a.component
-        a.component = () =>
-            import(`./views/${path}`)
-        if ('children' in e) {
-            a.children = setLayer(e.children)
-        }
-        return a
-    })
+  return obj.map(e => {
+    let a = e
+    let path
+    if (process.env.VUE_APP_TITLE == 'nolayout' && a.component == 'Main') {
+      path = 'RouterView'
+    }
+    else {
+      path = a.component
+    }
+
+    a.component = () =>
+      import(`./views/${path}`)
+    if ('children' in e) {
+      a.children = setLayer(e.children)
+    }
+    return a
+  })
 }
 setLayer(Routerdata.routerList)
 let router = Ea.RouterSet(Routerdata)
-console.log(router)
 new Vue({
-    router,
-    store,
-    render: h => h(App)
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
--- pages/views/Main.vue
+++ pages/views/Main.vue
@@ -6,14 +6,7 @@
       </div>
       <ea-sidemenu slot="sidemenu" />
       <ea-header slot="header">
-        <!-- 页签 -->
-        <ea-tagview slot="tagview" />
-       <!-- 右侧操作按钮 -->
-        <ea-refresh slot="options" />
-        <!-- <ea-help slot="options" />
-        <ea-fullscreen slot="options" />
-        <ea-pagesetting slot="options"/>
-        <ea-usercenter slot="options" /> -->
+
       </ea-header>
     </ea-layout>
   </div>
